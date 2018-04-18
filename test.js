98b485399cf5980d021a9acb25f6241d3141c0b3
Rohan Venkatapuram <sharpedo75@gmail.com>
2018-04-18 11:57:46 -0700

    yoit

--- test.js
+++ test.js
@@ -1,16 +1,53 @@
 $(document).ready(function() {
     // all custom jQuery will go here
-    $("#nameInput").keyup(function() {
-      $("p").text($("#nameInput").val());
-    })
+    const startWHeight = $(window).height();
+    let wHeight = $(window).height();
 
-    $("#nameInput").keydown(function() {
-      $("p").text($("#nameInput").val());
-    })
 
-    /*$("#myDiv").click(function() {
-      alert(getNameVal());
-      nope its pull time
 
-    })*/
+    const startWidth = $(window).width();
+    let width = $(window).width();
+
+    let scaler = Math.sqrt(wHeight / startWHeight * width / startWidth);
+
+    let fontSize = Math.round(50 * scaler);
+
+    let nameInput = $("#nameInput");
+    nameInput.width(Math.round(width / startWidth * 159) + "px");
+    nameInput.height(Math.round(wHeight / startWHeight * 20) + "px");
+
+
+
+    $("#myDiv").css("height", wHeight / 2 + "px");
+    $("#myDiv").css("margin-top", wHeight / 4 + "px");
+    $("#myDiv").css("font-size", fontSize + "px");
+
+    $(window).mousemove(function(event) {
+      wHeight = $(window).height();
+      width = $(window).width();
+      scaler = wHeight / startWHeight * width / startWidth;
+      fontSize = Math.round(50 * scaler);
+      $("#myDiv").css("height", $(window).height() / 2 + "px");
+      $("#myDiv").css("margin-top", wHeight / 4 + "px");
+      $("#myDiv").css("font-size", fontSize + "px");
+      //nameInput = $("#nameInput");
+      nameInput.width(Math.round(width / startWidth * 159) + "px");
+      nameInput.height(Math.round(wHeight / startWHeight * 20) + "px");
+
+    });
+
+
+
+
+//cool kid shadow effect
+    $("#myDiv").mouseover(function() {
+      //alert(`Font Size: ${fontSize} Scaler: ${scaler}`);
+      $("#myDiv").css("box-shadow", "0px 0px 100px");
+      $("#myDiv").css("-webkit-transition", "box-shadow 2s");
+      $("#myDiv").css("transition", "box-shadow 0.5s");
+    });
+
+    $("#myDiv").mouseleave(function() {
+      $("#myDiv").css("box-shadow", "");
+    });
 });
